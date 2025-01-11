024347f88dfb0bd52445bfa4b2c08ddcd1dc7ab3
Nguyen Trong Hieu <hieu745233@gmail.com>
2025-01-11 22:40:25 +0700

    feat: make header

---
+++ layouts/Default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Header from '@/components/molecules/Header.vue';
+</script>
+
+<template>
+  <div 
+    :style="{
+      padding: '0 2rem'
+    }"
+  >
+    <Header />
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
