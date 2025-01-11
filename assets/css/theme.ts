0a047b5c14dc41c806008f7170f38f13f5273712
Nguyen Trong Hieu <hieu745233@gmail.com>
2025-01-11 08:57:37 +0700

    feat: update code

--- assets/css/theme.ts
+++ assets/css/theme.ts
@@ -3,18 +3,17 @@ import Aura from '@primevue/themes/aura';
 
 export const Theme = definePreset(Aura, {
   semantic: {
-      primary: {
-          50: '{sky.50}',
-          100: '{sky.100}',
-          200: '{sky.200}',
-          300: '{sky.300}',
-          400: '{sky.400}',
-          500: '{sky.500}',
-          600: '{sky.600}',
-          700: '{sky.700}',
-          800: '{sky.800}',
-          900: '{sky.900}',
-          950: '{sky.950}'
-      }
+    primary: {
+      50: '#fff9e6', // Nhạt
+      100: '#fff3cc',
+      200: '#ffe999',
+      300: '#ffdf66',
+      400: '#ffc107', // Vàng đậm
+      500: '#e6ac06',
+      600: '#cc9805',
+      700: '#b38604',
+      800: '#997303',
+      900: '#805f02'
+    }
   }
 });
