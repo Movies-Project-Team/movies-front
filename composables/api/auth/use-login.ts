7f0746ecbb39f065d1ce2f04b85a010ed3d4c932
Nguyen Trong Hieu <hieu745233@gmail.com>
2025-01-16 23:53:29 +0700

    feat: update code

--- composables/api/auth/use-login.ts
+++ composables/api/auth/use-login.ts
@@ -1,61 +1,61 @@
-// import { useMutation } from "@tanstack/vue-query";
-// import { apiClient } from "~/utils/apiClient";
-// import { setCookie } from "~/utils/cookie";
-// import { useLoadingStore } from "~/stores/loading";
+import { useMutation } from "@tanstack/vue-query";
+import { apiClient } from "~/utils/apiClient";
+import { setCookie } from "~/utils/cookie";
+import { useLoadingStore } from "~/stores/loading";
 
-// const ENDPOINT = '/auth/login';
+const ENDPOINT = '/auth/login';
 
-// interface LoginPayload {
-//   username: string;
-//   password: string;
-// }
+interface LoginPayload {
+  username: string;
+  password: string;
+}
 
-// interface LoginResponse {
-//   id: number,
-//   username: string,
-//   email: string,
-//   firstName: string,
-//   lastName: string,
-//   gender: string,
-//   image: string,
-//   accessToken: string;
-//   refreshToken: string;
-// }
+interface LoginResponse {
+  id: number,
+  username: string,
+  email: string,
+  firstName: string,
+  lastName: string,
+  gender: string,
+  image: string,
+  accessToken: string;
+  refreshToken: string;
+}
 
-// const loginMutationFn = async (payload: LoginPayload) => {
-//   const api = apiClient();
-//   const response = await api<LoginResponse>(`${ENDPOINT}`, {
-//     method: 'POST',
-//     body: payload,
-//   });
+const loginMutationFn = async (payload: LoginPayload) => {
+  const api = apiClient();
+  const response = await api<LoginResponse>(`${ENDPOINT}`, {
+    method: 'POST',
+    body: payload,
+  });
+  
+  return response;
+};
 
-//   return response;
-// };
+export const useLogin = () => {
+  const loading = useLoadingStore();
 
-// export const useLogin = () => {
-//   const loading = useLoadingStore();
-
-//   const mutation = useMutation({
-//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//     mutationFn: loginMutationFn,
-//     onMutate: () => {
-//       loading.show();
-//     },
-//     onSuccess: (data: LoginResponse) => {
-//       const { accessToken } = data;
+  const mutation = useMutation({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    mutationFn: loginMutationFn,
+    onMutate: () => {
+      loading.show();
+    },
+    onSuccess: (data: LoginResponse) => {
+      const { accessToken } = data;
       
-//       if (accessToken) {
-//         setCookie('access_token', accessToken);
-//         loading.hide();
-//       }
-//     },
-//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//     onError: (error: any) => {
-//       loading.hide();
-//     },
-//   });
+      if (accessToken) {
+        setCookie('access_token', accessToken);
+        loading.hide();
+      }
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    onError: (error: any) => {
+      loading.hide();
+    },
+  });
 
-//   return {
-//     ...mutation,
-//   };
-// };
+  return {
+    ...mutation,
+  };
+};
